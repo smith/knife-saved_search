9ed005764855d266d2538c1ca2d93a139e6f07a9
Nathan L Smith <smith@chef.io>
2015-04-03 12:31:15 -0700

    add plugin file; remove original class

---
+++ lib/chef/knife/saved_search.rb
@@ -0,0 +1,13 @@
+require 'chef/knife'
+
+class Chef
+  class Knife
+    class SavedSearch < Knife
+      banner "knife saved search [options]"
+
+      def run
+
+      end
+    end
+  end
+end
--- lib/knife/saved_search.rb
+++
@@ -1,7 +0,0 @@
-require "knife/saved_search/version"
-
-module Knife
-  module SavedSearch
-    # Your code goes here...
-  end
-end
